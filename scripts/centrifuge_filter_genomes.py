281c746ce76f30081333924bc519611d7776175e
Kange2014 <mingkanghust@gmail.com>
2019-09-18 16:44:28 +0800

    Add files via upload

--- scripts/centrifuge_filter_genomes.py
+++ scripts/centrifuge_filter_genomes.py
@@ -36,18 +36,25 @@ def filter_genomes(input,db,mapfile,min_read_count,min_abundance,host_taxid,outp
     filtered.to_csv(output, sep="\t", index=True)
 
 
-def main(args):
-    filter_genomes(args.input, args.db, args.mapfile, args.min_read_count, args.min_abundance, args.host_taxid, args.output)
+#def main(args):
+    #filter_genomes(args.input, args.db, args.mapfile, args.min_read_count, args.min_abundance, args.host_taxid, args.output)
 
+## -i snakemake.input[0] -d snakemake.input[1] -M snakemake.input[2] -r snakemake.params[0] 
+## -a snakemake.params[1] -t snakemake.params[2] -o snakemake.output[0]
+
+def main():
+    filter_genomes(snakemake.input[0],snakemake.input[1],snakemake.input[2],snakemake.params[0],\
+                   snakemake.params[1],snakemake.params[2],snakemake.output[0])
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("-i", "--input", type=str, required=True)
-    parser.add_argument("-d", "--db", type=str, required=True)
-    parser.add_argument("-r", "--min_read_count", type=int, required=True)
-    parser.add_argument("-a", "--min_abundance", type=float, required=True)
-    parser.add_argument("-t", "--host_taxid", type=str, required=True)
-    parser.add_argument("-M", "--mapfile", type=str, required=True)
-    parser.add_argument("-o", "--output", type=str, required=True)
-    args = parser.parse_args()
-    main(args)
+    #parser = ArgumentParser()
+    #parser.add_argument("-i", "--input", type=str, required=True)
+    #parser.add_argument("-d", "--db", type=str, required=True)
+    #parser.add_argument("-r", "--min_read_count", type=int, required=True)
+    #parser.add_argument("-a", "--min_abundance", type=float, required=True)
+    #parser.add_argument("-t", "--host_taxid", type=str, required=True)
+    #parser.add_argument("-M", "--mapfile", type=str, required=True)
+    #parser.add_argument("-o", "--output", type=str, required=True)
+    #args = parser.parse_args()
+    #main(args)
+    main()
